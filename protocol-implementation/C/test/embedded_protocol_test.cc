0df322eaf7fbc6b6306f90cc3ce7284821625ed4
budgettsfrog <budgettsfrog@protonmail.com>
2022-08-22 12:33:15 -0700

    Filling out user stubs for unit testing. Got a print and notify global

--- protocol-implementation/C/test/embedded_protocol_test.cc
+++ protocol-implementation/C/test/embedded_protocol_test.cc
@@ -2,15 +2,6 @@
 #include "../src/serial_protocol.h"
 #include "../src/user_impl.h"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions)
-{
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
-}
-
 TEST(Message_Building, Valid_Header_No_Payload)
 {
   uint8_t test_addr = 0;
@@ -20,4 +11,19 @@ TEST(Message_Building, Valid_Header_No_Payload)
 
   // Assert the message was queued successfuly
   ASSERT_EQ(ret, SP_OK);
+
+  // Assert the tx callback was called
+  extern uint8_t global_tx_notify;
+  ASSERT_TRUE(global_tx_notify);
+
+  // Assert the message was populated properly
+  extern uint8_t global_tx_buff[RX_BUFFER_LEN];
+  ASSERT_EQ(global_tx_buff[HEADER_0_POS], HEADER_0_VAL);
+  ASSERT_EQ(global_tx_buff[HEADER_1_POS], HEADER_1_VAL);
+  ASSERT_EQ(global_tx_buff[HEADER_2_POS], HEADER_2_VAL);
+  ASSERT_EQ(global_tx_buff[SRC_ADDR_POS], MY_ADDR);
+  ASSERT_EQ(global_tx_buff[TGT_ADDR_POS], test_addr);
+  ASSERT_EQ(global_tx_buff[MSG_TYPE_POS], test_type);
+  ASSERT_EQ(global_tx_buff[PAYLOAD_LEN_MSB_POS], (uint8_t)(test_payload_len << 8));
+  ASSERT_EQ(global_tx_buff[PAYLOAD_LEN_LSB_POS], (uint8_t)(test_payload_len & 0xFF));
 }
--- protocol-implementation/C/test/user_impl.c
+++ protocol-implementation/C/test/user_impl.c
@@ -1,12 +1,15 @@
 #include "../src/user_impl.h"
 #include "../src/serial_protocol.h"
+#include <string.h>
+#include <printf.h>
 
 // RX Types
 #define RX_TYPE_0 0
 #define RX_TYPE_1 1
 #define RX_TYPE_2 2
 
-typedef struct {
+typedef struct
+{
     uint8_t type;
     uint16_t length;
 } message_descriptor_t;
@@ -15,8 +18,23 @@ message_descriptor_t rx_type_0 = {RX_TYPE_0, 0};
 message_descriptor_t rx_type_1 = {RX_TYPE_1, MAX_PAYLOAD_LEN};
 message_descriptor_t rx_type_2 = {RX_TYPE_2, 8};
 
-void user_rcv_message( uint8_t *msg_buf, uint32_t msg_len )
-{}
+void user_rcv_message(uint8_t *msg_buf, uint32_t msg_len)
+{
+}
 
-void user_serial_tx( uint8_t *msg_buf, uint32_t msg_len )
-{}
+// Global buffer and tx notifitcaion
+uint8_t global_tx_buff[RX_BUFFER_LEN];
+uint8_t global_tx_notify = 0;
+void user_serial_tx(uint8_t *msg_buf, uint32_t msg_len)
+{
+    // Copy the message into the global notify
+    memset(global_tx_buff, 0, RX_BUFFER_LEN);
+    memcpy(global_tx_buff, msg_buf, msg_len);
+    global_tx_notify = 1;
+
+    // Print the message
+    printf("user_serial_tx sending:");
+    for (uint32_t i = 0; i < msg_len; i++)
+        printf("%X", msg_buf[i]);
+    printf("\n");
+}
