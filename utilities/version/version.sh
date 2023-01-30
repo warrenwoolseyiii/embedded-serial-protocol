1985e1d1ce73ef998c8b108787cd57a94290ccc7
budgettsfrog <budgettsfrog@protonmail.com>
2023-01-30 11:33:39 -0800

    Version info (#16)
    * Added a versioning script that reads the version from a single JSON file and distributes to python and C
    
    * version information working properly
    
    * Increment rev field in version.json
    
    * updated for git hooks
    
    * test pre-commit
    
    * test pre-commit1
    
    * Increment rev field in version.json
    
    * Increment rev field in version.json
    
    * Increment rev field in version.json
    
    * Increment rev field in version.json
    
    * Increment rev field in version.json
    
    * Increment rev field in version.json
    
    * Increment rev field in version.json
    
    * Increment rev field in version.json
    
    * precommit hooks working
    
    * formatted python imp
    
    * format the kotlin lib
    
    * Increment rev field in version.json
    
    * Increment rev field in version.json
    
    * testing
    
    * testing
    
    * Okay I think it is working now
    
    * formatting properly

--- utilities/version/version.sh
+++ utilities/version/version.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # The input JSON file
-input_file="version.json"
+input_file="utilities/version/version.json"
+tmp_file="utilities/version/version.json.tmp"
+
+### Bump the version ###
+# Increment the rev field in the version.json file
+jq ".VERSION_REV += 1" $input_file > $tmp_file
+mv $tmp_file $input_file
 
 ### C ###
 
 # The output header file
-c_out_file="../../protocol-implementation/C/src/version.h"
+c_out_file="protocol-implementation/C/src/version.h"
 
 # Parse the JSON file to extract the constants
 major=$(cat $input_file | jq -r '.VERSION_MAJOR')
@@ -26,7 +32,7 @@ echo "#endif /* VERSION_H_ */" >> $c_out_file
 ### Python ###
 
 # The output Python file
-python_outfile="../../protocol-implementation/Python/version.py"
+python_outfile="protocol-implementation/Python/version.py"
 
 # Parse the JSON file to extract the constants
 major=$(cat $input_file | jq -r '.VERSION_MAJOR')
@@ -41,7 +47,7 @@ echo "VERSION_REV = $rev" >> $python_outfile
 ### Kotlin ###
 
 # The output Kotlin file
-kotlin_outfile="../../protocol-implementation/Kotlin/serial-protocol/src/main/kotlin/serial/protocol/Version.kt"
+kotlin_outfile="protocol-implementation/Kotlin/serial-protocol/src/main/kotlin/serial/protocol/Version.kt"
 
 # Parse the JSON file to extract the constants
 major=$(cat $input_file | jq -r '.VERSION_MAJOR')
@@ -56,14 +62,20 @@ echo "    const val VERSION_REV = $rev" >> $kotlin_outfile
 echo "}" >> $kotlin_outfile
 
 # Read the version fields from the JSON file
-version_major=$(jq -r '.VERSION_MAJOR' version.json)
-version_minor=$(jq -r '.VERSION_MINOR' version.json)
-version_rev=$(jq -r '.VERSION_REV' version.json)
+version_major=$(jq -r '.VERSION_MAJOR' $input_file)
+version_minor=$(jq -r '.VERSION_MINOR' $input_file)
+version_rev=$(jq -r '.VERSION_REV' $input_file)
 
 # Create the version string
 version="$major.$minor.$rev"
 
 # Insert the version string into the build.gradle.kts file
-sed -i "s/version = \".*\"/version = \"$version\"/" ../../protocol-implementation/Kotlin/serial-protocol/build.gradle.kts
+sed -i "s/version = \".*\"/version = \"$version\"/" protocol-implementation/Kotlin/serial-protocol/build.gradle.kts
 
-
+# Add and commit the version.json file
+#./../../protocol-implementation/Kotlin/gradlew build
+git add protocol-implementation/Kotlin/serial-protocol/build.gradle.kts
+git add $input_file
+git add $python_outfile
+git add $c_out_file
+git add $kotlin_outfile
