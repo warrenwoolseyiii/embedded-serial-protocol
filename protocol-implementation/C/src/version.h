edf6d3528b3a018193d359472f144fca606cc0a4
budgettsfrog <budgettsfrog@protonmail.com>
2023-01-30 11:10:01 -0800

    Increment rev field in version.json

--- protocol-implementation/C/src/version.h
+++ protocol-implementation/C/src/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 0
-#define VERSION_REV 2
+#define VERSION_MAJOR 
+#define VERSION_MINOR 
+#define VERSION_REV 
 
 #endif /* VERSION_H_ */
