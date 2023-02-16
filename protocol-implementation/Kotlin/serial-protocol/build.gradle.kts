7b2a567e0678ce624f70e92634a31293a5dc7d25
budgettsfrog <budgettsfrog@protonmail.com>
2023-02-15 19:30:40 -0800

    Change C release back to main branch

--- protocol-implementation/Kotlin/serial-protocol/build.gradle.kts
+++ protocol-implementation/Kotlin/serial-protocol/build.gradle.kts
@@ -7,7 +7,7 @@
  */
 
 group = "com.warrenwoolseyiii"
-version = "1.1.17"
+version = "1.1.18"
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.6.21"
